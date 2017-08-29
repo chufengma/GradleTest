eb1c95cf6ba13cd6b3e20bfd961e32dbc412b0e1
yfchu <yfchu@Ctrip.com>
2017-08-29 11:24:30 +0800

    update

--- code/PluginTest2/src/main/groovy/com/onefengma/hello/HelloPlugin.groovy
+++ code/PluginTest2/src/main/groovy/com/onefengma/hello/HelloPlugin.groovy
@@ -6,11 +6,27 @@ import org.gradle.api.Plugin;
 public class HelloPlugin implements Plugin<Project> {
 
     void apply(Project project) {
+
         project.task('hello') {
             doLast {
+            	if (false) {
+            		println "HAHAHA false"
+            	} else {
+            		println "HAHAHA true"            		
+            	}
+
                 println "Hello from the GreetingPlugin"
             }
         }
+
+        project.configurations {
+            hello
+        }
+
+        project.ext.helloName = ""
+
+        
+
     }
     
 }
